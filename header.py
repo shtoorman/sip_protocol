d59ab1fcacaf5e433e35de5f83d8d6682b5cd736
Aleksandr Balandin <shtoorman2683@gmail.com>
2019-07-03 14:28:33 +0300

    add chenges

---
+++ header.py
@@ -0,0 +1,80 @@
+from message import Message
+from address import Address
+
+
+class Header(object):
+
+    def _parse(self, value, name):
+        if name in Message._address:  # address header
+            addr = Address();
+            addr.mustQuote = True
+        count = addr.parse(value)
+        value, rest = addr, value[count:]
+        if rest:
+            for n, sep, v in map(lambda x: x.partition('='), rest.split(';') if rest else []):
+                if n.strip():
+                    self.__dict__[n.lower().strip()] = v.strip()
+        elif name not in Message._comma and name not in Message._unstructured:  # standard
+            value, sep, rest = value.partition(';')
+            for n, sep, v in map(lambda x: x.partition('='), rest.split(';') if rest else []):
+                self.__dict__[n.lower().strip()] = v.strip()
+
+        if name in Message._comma:
+            self.authMethod, sep, rest = value.strip().partition(' ')
+            for n, v in map(lambda x: x.strip().split('='), rest.split(',') if rest else []):
+                self.__dict__[n.lower().strip()] = Message._unquote(v.strip())
+
+        elif name == 'cseq':
+            n, sep, self.method = map(lambda x: x.strip(), value.partition(' '))
+            self.number = int(n)
+            value = n + ' ' + self.method
+
+    def __init__(self, value=None, name=None):
+        self.name = name and Message._canon(name.strip()) or None
+        self.value = Header._parse(value.strip(), self.name and self.name.lower() or None)
+
+    def __str__(self):
+        name = self.name.lower()
+
+        rest = '' if ((name in Message._comma) or (name in Message._unstructured)) \
+            else (';'.join(map(lambda x: self.__dict__[x] and '%s=%s' % (x.lower(), self.__dict__[x]) or x,
+                               filter(lambda x: x.lower() not in ['name', 'value', '_viauri'],
+                                      self.__dict__))))
+        return str(self.value) + (rest and (';' + rest) or '')
+
+    def __repr__(self):
+        return self.name + ": " + str(self)
+
+    def dup(self):
+        return Header(self.__str__(), self.name)
+
+    def __getitem__(self, name):
+        return self.__dict__.get(name.lower(), None)
+
+    def __setitem__(self, name, value):
+        self.__dict__[name.lower()] = value
+
+    def __contains__(self, name):
+        return name.lower() in self.__dict__
+
+    @property
+    def viaUri(self):
+        if not hasattr(self, '_viaUri'):
+            if self.name != 'Via':
+                raise ValueError('viaUri available only on Via header')
+
+    proto, addr = self.value.split(' ')
+    type = proto.split('/')[2].lower()  # udp, tcp, tls
+    self._viaUri = URI('sip:' + addr + ';transport=' + type)
+    if self._viaUri.port == None: self._viaUri.port = 5060
+    if 'rport' in self:
+        try:
+            self._viaUri.port = int(self.rport)
+        except:
+            pass  # probably not an int
+    if type not in ['tcp', 'sctp', 'tls']:
+        if 'maddr' in self:
+            self._viaUri.host = self.maddr
+        elif 'received' in self:
+            self._viaUri.host = self.received
+    return self._viaUri
--- message.py
+++ message.py
@@ -1,4 +1,6 @@
 class Message(object):
+
+
     _address = ['contact', 'from', 'record-route', 'refer-to', 'referred-by', 'route', 'to']
     _comma = ['authorization', 'proxy-authenticate', 'proxy-authorization', 'www-authenticate']
     _unstructured = ['call-id', 'cseq', 'date', 'expires', 'max-forwards', 'organization', 'server', 'subject',
@@ -8,10 +10,44 @@ class Message(object):
               'o', 'from', 'f', 'subject', 's', 'supported', 'k', 'to', 't', 'via', 'v']
     _exception = {'call-id': 'Call-ID', 'cseq': 'CSeq', 'www-authenticate': 'WWW-Authenticate'}
 
+    _quote = lambda s: '"' + s + '"' if s[0] != '"' != s[-1] else s
+    _unquote = lambda s: s[1:-1] if s[0] == '"' == s[-1] else s
+
+    _keywords = ['method', 'uri', 'response', 'responsetext', 'protocol', '_body', 'body']
+    _single = ['call-id', 'content-disposition', 'content-length', 'content-type', 'cseq', 'date', 'expires', 'event',
+               'max-forwards',
+               'organization', 'refer-to', 'referred-by', 'server', 'session-expires', 'subject', 'timestamp', 'to',
+               'user-agent']
+    
     def _canon(s):
         s = s.lower()
 
-    # a = ((len(s) == 1) and s in _short and _canon(_short[_short.index(s) - 1]))
-    # print(a)
-        return (((len(s) == 1) and s in _short and _comma(_short[_short.index(s) - 1])) or (
-            s in _exception and _exception[s]) or '-'.join([x.capitalize() for x in s.split('-')]))
+        return ((len(s) == 1) and s in Message._short and Message._comma(
+            Message._short[Message._short.index(s) - 1])) or (
+                        s in Message._exception and Message._exception[s]) or '-'.join(
+            [x.capitalize() for x in s.split('-')])
+
+    def __getattr__(self, name):
+        return self.__getitem__(name)
+
+    def __getattribute__(self, name): return object.__getattribute__(self, name.lower())
+
+    def __setattr__(self, name, value): object.__setattr__(self, name.lower(), value)
+
+    def __delattr__(self, name):
+        object.__delattr__(self, name.lower())
+
+    def __hasattr__(self, name):
+        object.__hasattr__(self, name.lower())
+
+    def __getitem__(self, name):
+        return self.__dict__.get(name.lower(), None)
+
+    def __setitem__(self, name, value): self.__dict__[name.lower()] = value
+
+    def __contains__(self, name):
+        return name.lower() in self.__dict__
+
+print(Message._canon('call-Id'))
+print(Message._canon('fRoM'))
+print(Message._canon('refer-to'))
